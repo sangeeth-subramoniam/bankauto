b39cb8c6cf5e0d47cd0c01c2debec0cc46835e0b
unknown <s-sangeeth-k@sicis.co.jp>
2021-05-06 14:15:25 +0900

    limit character of the  content displayed in jumbotron of apprved images

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = 'homepage'
+
 urlpatterns = [
     path('', views.home , name = "home"),
     path('contact', views.contact , name = "contact"),
